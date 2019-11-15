113bad1c2797f848abf9ca4e1f50b0f23b18c6e6
Michael Buchler <michael.buchler@sapconet.co.za>
2019-11-15 13:50:54 +0200

    Initial Commit of OpenALPR project

---
+++ src/main/java/OpenALPR_Test.java
@@ -0,0 +1,59 @@
+import com.openalpr.jni.Alpr;
+import com.openalpr.jni.AlprException;
+import com.openalpr.jni.AlprPlate;
+import com.openalpr.jni.AlprPlateResult;
+import com.openalpr.jni.AlprResults;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class OpenALPR_Test {
+    private static Alpr alpr;
+    public static void main(String[] args) {
+        alpr = new Alpr("eu", "C:/Users/cwjvr/Documents/openalpr_64/runtime_data/config/openalpr.conf", "C:/Users/cwjvr/Documents/openalpr_64/runtime_data");
+
+
+
+        // Set top N candidates returned to 20
+        alpr.setTopN(5);
+
+        // Attempt to detect a region
+        alpr.setDefaultRegion("de");
+        AlprResults results = null;
+        try {
+            Path path = Paths.get("C:/Users/cwjvr/Documents/openalpr_64/samples/kr-1.jpg");
+            byte[] imagedata = Files.readAllBytes(path);
+            results = alpr.recognize(imagedata);
+        } catch (AlprException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        assert results != null;
+        System.out.print(results.getImgWidth());
+        System.out.format("  %-15s%-8s\n", "Plate Number", "Confidence");
+
+        for (AlprPlateResult result : results.getPlates()) {
+            for (AlprPlate plate : result.getTopNPlates()) {
+                if (plate.isMatchesTemplate())
+                    System.out.print("  * ");
+                else
+                    System.out.print("  - ");
+
+                System.out.format("%-15s%-8f\n", plate.getCharacters(), plate.getOverallConfidence());
+
+            }
+
+        }          // Make sure to call this to release memory
+
+        alpr.unload();
+    }
+
+    private static void readFiles(String dir) {
+
+    }
+}
